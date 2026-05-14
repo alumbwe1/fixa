8fb19a071178c09075ba978381a0dc88bc6088a6
alumbwe1 <alookscodes@gmail.com>
2026-05-14 22:36:07 +0200

    fixed svg error

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_strings.dart';
 import '../../../core/utils/app_style.dart';
